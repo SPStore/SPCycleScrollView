b2cbc779055256faa5ac90e60bc42a846ef22d0d
SPStore <libo@libodemacbook-pro.local>
2017-05-04 09:03:24 +0800

    Initial Commit

---
+++ SPCarouselView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SPCarouselView
+//
+//  Created by Libo on 17/5/4.
+//  Copyright © 2017年 iDress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SPCarouselView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SPCarouselView
+//
+//  Created by Libo on 17/5/4.
+//  Copyright © 2017年 iDress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
